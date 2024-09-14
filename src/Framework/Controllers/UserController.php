a0de3583c03a9095466d5e261348e0f9ca6d4eee
Csekme Krisztián <csekme.krisztian@outlook.com>
2024-09-14 21:40:42 +0200

    working on framework

--- src/Framework/Controllers/UserController.php
+++ src/Framework/Controllers/UserController.php
@@ -2,23 +2,33 @@
 
 namespace Framework\Controllers;
 
+use Framework\AbstractController;
 use Framework\Controller;
 use Framework\HasRoles;
 use Framework\Models\User;
 use Framework\Path;
 use Framework\RequireLogin;
 use Framework\Response;
-use Framework\View;
 
 #[RequireLogin]
 #[HasRoles(['ROLE_ADMIN'])]
 #[Path('/user')]
 class UserController extends Controller
 {
+    #[Path('/getAll', method: AbstractController::GET)]
     public function indexAction(): Response
     {
         $users = User::findAll();
         return $this->view('User/users.twig', [ "users" => $users ]);
+    }
+
+    #[Path('/save', method: AbstractController::POST)]
+    public function saveAction(): Response
+    {
+        $user = new User($this->request->post);
+        if ($user->save()) {
+            $this->redirect('/user');
+        }
     }
 
 
